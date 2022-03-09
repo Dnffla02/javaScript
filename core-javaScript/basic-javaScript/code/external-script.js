c24d10c55457a4ad79326fa046a75c65fc0ca60e
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-09 18:05:28 +0900

    Create external-script.js

---
+++ core-javaScript/basic-javaScript/code/external-script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+<!DOCTYPE HTML>
+<html>
+  <body>
+    <p>스크립트 전</p>
+    <script src="/path/to/script1.js>//해당 위치의 js파일 ,,
+    </script>
+    <script src="/path/to/script2.js>//해당 위치의 복수 js 파일
+    </script>
+  </body>
+</html>
+
+
+
+
+
+
