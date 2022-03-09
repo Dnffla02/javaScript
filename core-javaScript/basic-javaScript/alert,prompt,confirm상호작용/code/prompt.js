e6045aab92aa273047a6a725702ef21d8a53e8ae
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-10 02:59:52 +0900

    Update prompt.js

--- core-javaScript/basic-javaScript/alert,prompt,confirm상호작용/code/prompt.js
+++ core-javaScript/basic-javaScript/alert,prompt,confirm상호작용/code/prompt.js
@@ -6,8 +6,13 @@
 result=prompt(title,[default]);
 //title: 사용자에게 보여줄 문자열
 //default: 입력필드의 초깃값(선택)
-
-
+let age=prompt('나이값 입력',100);
+alert("당신의 나이는 &{age} 입니다.");
+/*
+항상 기본값을 넣어야함
+let test=prompt("Test");//안됨
+let test=prompt("Test",'');//
+*/
 
 
 
