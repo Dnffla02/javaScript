d26b13b6e3ba2ceaf14496d6644b734bd045ec28
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-10 02:46:35 +0900

    Create string.js

---
+++ core-javaScript/basic-javaScript/data-type/code/string.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+let str="Hello";
+let str2='Single quotes are ok too';
+let phrase=`can embed another &{str}`;
+
+let name="John";
+alert('Hello, &{name}');
+alert('the result is &{1+2}');
+
+alert("the result is &{1+2}");
+
+
+
+
+
+
+
+
