53354b62118182725a685f49ac659f8d08d79cb9
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-10 02:50:47 +0900

    Create typeof.js

---
+++ core-javaScript/basic-javaScript/data-type/code/typeof.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+typeof undefined//undefined
+typeof 0//number
+typeof 10n//bigint
+typeof ture//boolean
+typeof "foo"//string
+typeof Symbol("id")// symbol
+typeof Math//object
+typeof null//object
+typeof alert//function
+
+
+
+
+
+
+
+
+
+
+
+
