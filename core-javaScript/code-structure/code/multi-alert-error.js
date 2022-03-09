0140c511cb9c2da0f4ad14fe847a602c18a2d6fe
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-09 18:28:27 +0900

    Create multi-alert-error.js

---
+++ core-javaScript/code-structure/code/multi-alert-error.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+[1,2].forEach(alert) // 1,2출력
+
+alert("에러가 발생합니다.")
+[1,2].forEach(alert) // alert만 출력
+
+alert("에러가 발생합니다.");
+[1,2].forEach(alert) // alert, 1, 2 출력
+
+alert("에러가 발생합니다.")[1, 2].forEach(alert) // alert만 출력
+
+
+
+
+
