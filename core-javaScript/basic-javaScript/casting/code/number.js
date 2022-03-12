12d16832864c531955798f4ede506da23206ca96
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-12 19:56:34 +0900

    Create number.js

---
+++ core-javaScript/basic-javaScript/casting/code/number.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+alert("6"/"2");
+
+
+let str="123";
+alert(typeof str);//string
+
+let num=Number(str);
+
+alert(typeof num);//number
+
+
+let age=Number("임의의 문자열 123");
+
+alert(age);//NaN형, 변환 실패
+
+
+alert(Number("   123   "));//123
+alert(Number("123z");//NaN
+alert(Number(true));//1
+alert(Number(false));//0
+
+
+
+
+
+
+
+
+
+
