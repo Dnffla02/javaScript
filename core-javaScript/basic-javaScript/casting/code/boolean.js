b0a257ca151082edee8b880f1593592eb6c70ec1
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-12 20:38:44 +0900

    Create boolean.js

---
+++ core-javaScript/basic-javaScript/casting/code/boolean.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+alert(Boolean(1));//1
+alert(Boolena(0));//0
+
+alert(Boolean("Hello");//1
+alert(Boolean(""));//0
+
+
+//*문자열 "0"은 true임
+alert(Boolean("0");//1
+//*문자열 공백도 문자열이여서 true
+alert(Boolean(" ");//1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
