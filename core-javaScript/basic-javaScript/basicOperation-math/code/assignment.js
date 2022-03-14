56aef311bcf7091decc6f9780ae08031fb0b07e4
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-14 09:24:19 +0900

    Create assignment.js

---
+++ core-javaScript/basic-javaScript/basicOperation-math/code/assignment.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+let x=2*2+1;
+
+alert(x);
+
+
+let a=1;
+let b=2;
+
+let c=3-(a=b+1);
+
+alert(a);//3
+alert(c);//0
+
+
+let a,b,c;
+a=b=c=2+2;
+
+alert(a);//4
+alert(b);
+
+
+c=2+2;
+b=c;
+a=c;
+
+
+
+
+
+
+
+
+
+
+
+
+
