5603cc9bd276b6fe6288d592281702efe4c2d797
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-14 09:12:58 +0900

    Create nuaryOperation-plug.js

---
+++ core-javaScript/basic-javaScript/basicOperation-math/code/nuaryOperation-plug.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+let x=1;
+alert(+x);//1
+
+let y=-2;
+alert(+);//-2
+      
+alert(+true);//1
+alert(+"");//0
+
+
+let apples="2";
+let oranges="3";
+
+alert(apples+oranges);//2
+
+
+alert(+apples++oranges);//5
+
+
+
+
+
+
+
+
+
+
