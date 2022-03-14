b3e86e26152aed9c7bb4c1209c19b209aea756aa
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-14 11:21:47 +0900

    Update increment-decrement_operation.js

--- core-javaScript/basic-javaScript/basicOperation-math/code/increment-decrement_operation.js
+++ core-javaScript/basic-javaScript/basicOperation-math/code/increment-decrement_operation.js
@@ -13,7 +13,41 @@ counter--;
 alert(counter);
 
 
+counter=1;
+let a=++counter;
+alert(a);//2
 
+
+counter=1;
+a=counter++;
+alert(a);//1
+
+
+counter=0;
+counter++;
+++counter;
+alert(counter);//2
+
+
+counter=0;
+alert(++counter);//1
+
+
+counter=0;
+alert(counter++);//0
+
+
+counter=1;
+alert(2*++counter);
+
+
+counter=1;
+alert(2*counter++);//2
+
+
+conter=1;
+alert(2*counter);
+counter++;
 
 
 
