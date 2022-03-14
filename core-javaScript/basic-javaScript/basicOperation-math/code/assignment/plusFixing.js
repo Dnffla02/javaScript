efd1bbd67cf8167ae5a2b5dd47a1dbc18d32a56b
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-14 11:34:06 +0900

    Create plusFixing.js

---
+++ core-javaScript/basic-javaScript/basicOperation-math/code/assignment/plusFixing.js
@@ -0,0 +1,29 @@
+
+
+
+let a=prompt("숫자입력1",1);
+let b=prompt("숫자입력2",2);
+
+alert(a+b);//12
+
+
+let a=prompt("숫자입력1",1);
+let b=prompt("숫자입력2",2);
+
+alert(+a++b);//3
+
+
+let a=+prompt("숫자입력1",1);
+let b=+prompt("숫자입력2",2);
+
+alert(a+b);//3
+
+
+
+
+
+
+
+
+
+
